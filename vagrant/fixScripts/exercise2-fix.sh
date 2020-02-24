d8d2b303bdfc77a2f0d2ef22afb41cb5ddc25bcb
eranmos <moshayov.eran@thomsonreuters.com>
2020-02-24 15:28:34 +0200

    adding solutions of the exercises

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise2 here
+sudo sed -i.bak '/www.ascii-art.de/d' /etc/hosts
--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
+sudo sed -i ':a;N;$!ba;s/deny from all/allow from all/1' /etc/apache2/sites-available/default
+sudo service apache2 restart
